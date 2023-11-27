378013e54d84290580ae7b38f1d2a0bb42414ed7
Sean Sundberg <seansund@us.ibm.com>
2023-11-27 11:20:22 -0600

    Return absolute url to document
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- src/util/build-document-url.ts
+++ src/util/build-document-url.ts
@@ -1,4 +1,6 @@
 
+const host = process.env.DOC_URL_BASE || 'https://digital-pds-ui.19lft24df6mk.us-south.codeengine.appdomain.cloud/api'
+
 export const buildDocumentUrl = (id: string, doc: {id: string, name: string}): string => {
-    return `/family-allowance/${id}/doc/${doc.id}/${doc.name}`
+    return `${host}/family-allowance/${id}/doc/${doc.id}/${doc.name}`
 }
